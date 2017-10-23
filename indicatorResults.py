cf46d2b155bd3457de41734104fa43cf4bb6afd4
joysss <jhabb071@uottawa.ca>
2017-10-23 17:19:58 -0400

    strategies file column names

--- indicatorResults.py
+++ indicatorResults.py
@@ -5,17 +5,34 @@ class INDICATORRESULTS():
     def main():
         print('start automation\n')
         print('reading data source\n')
-        wb1 = load_workbook(filename = 'data-source.xlsx')
+        wb1 = load_workbook(filename = '..\data-source.xlsx')
+        ws1 = wb1.active
         print('reading indicator template\n')
-        wb2 = load_workbook(filename = 'indicator-template.xlsx')
+        wb2 = load_workbook(filename = '..\indicator-template.xlsx')
+        ws2 = wb2.active
 
         sheet_ranges = wb2['Indicators']
-        print(sheet_ranges['D22'].value)
 
-        with open('strategie-results.csv', 'wb') as csvfile:
+        colValues = {}
+        for row in ws2.iter_rows(min_row=3, max_col=1, max_row=25):
+            for cell in row:
+                colValues[cell] = cell.value
+
+        for cols in ws1:
+            for cell in cols:
+                if cell.value == "Date":
+                    print(cell.value)
+            
+
+        with open('..\strategie-results.csv', 'w', newline='') as csvfile:
             filewriter = csv.writer(csvfile, delimiter=',',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    
+                                    quotechar='|', lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
+
+
+        
+        
+        print('result file created\n')
+
 
     if __name__ == "__main__": main()
  
